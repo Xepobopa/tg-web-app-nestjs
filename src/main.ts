7cb0e4f79c677ec6565bc57641fe5867578f4b6c
Xepobopa <ismartsdn4477@gmail.com>
2023-08-11 15:10:57 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -12,18 +12,18 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
 
     const router = server._events.request._router;
-    const availableRoutes: [] = router.stack
-        .map(layer => {
+    const availableRoutes: { route: { path: string; method: string } }[] = router.stack
+        .map((layer: Layer) => {
             if (layer.route) {
                 return {
                     route: {
-                        path: layer.route?.path,
-                        method: layer.route?.stack[0].method
+                        path: layer.route.path,
+                        method: layer.route.stack[0].method
                     }
                 };
             }
         })
-        .filter(item => item !== undefined);
+        .filter(item => item !== undefined) as any; // You might need to use 'as any' here to handle filter resultv
     console.log(availableRoutes);
 
     await eduCompanion.start();
